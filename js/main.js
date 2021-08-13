06a55ccad61553d111a57761e9cb9a6e10c79523
chamoElias0 <81429791+chamoElias0@users.noreply.github.com>
2021-08-12 23:03:56 -0300

    entregable chamorro Jquery

--- js/main.js
+++ js/main.js
@@ -19,77 +19,65 @@ if ("productos" in localStorage){
 // productos.push(new Producto(8,"Camperita","Unico" , "Rosa" , '../img/tricoloe2.jfif',2700));
 // productos.push(new Producto(9,"Camperita","Unico" , "Gris" ,'../img/tricolor3.jfif', 2700));
 
-for(const producto of productos){
-    const productList = document.getElementById('product-list');
-    const element = document.createElement('div');
-    element.innerHTML = `<div class= "card text-center mb-4">
-                                <div class= "card-body">
-                                    <strong>Tipo Producto</strong>: ${producto.tipo}
-                                    <br>
-                                    <strong>Talle Producto</strong>: ${producto.talle}
-                                    <br>
-                                    <strong>Color Producto</strong>: ${producto.color}
-                                    <br>
-                                    <strong>Precio Producto</strong>: ${producto.precio}
-                                    <button type="button" id="${producto.id}" name="delete" class="btnCompra btn-primary">Anadir al carrito</button>
 
-                                <a href="#" class = "btn btn-danger">Borrar</a>
-                        </div>
-                        `
-    productList.appendChild(element);
-    //  deleteProduct(){
-    //  };
- 
-}
 
 const botones = document.getElementsByClassName('btnCompra');
 console.log(botones);
 
+// ----------------------agregue Jquery----------------------------------------------------------------
+function productosUIjQuery(productos , id){
+    for (const producto of productos) {
+        $(id).append(`<div class= "card text-center mb-4">
+        <div class= "card-body">
+            <strong>Tipo Producto</strong>: ${producto.tipo}
+            <br>
+            <strong>Talle Producto</strong>: ${producto.talle}
+            <br>
+            <strong>Color Producto</strong>: ${producto.color}
+            <br>
+            <strong>Precio Producto</strong>: ${producto.precio}
+            <button type="button" id="${producto.id}" name="delete" class="btnCompra btn-primary">Anadir al carrito</button>
+
+        <a href="#" class = "btn btn-danger">Borrar</a>
+</div>`)
+    }
+}
+productosUIjQuery(productos , "#product-list");
+
+
+
+
 
 const carrito = [];
 
-// function comprarManejador(e){
-//     // console.log(e.target.id);
-//     const seleccionado = productos.find( producto => producto.id == this.id);
-//     carrito.push(seleccionado);
-//     console.log(seleccionado);
-//     localStorage.setItem('seleccionado' , JSON.stringify(carrito)); //nos habilita a guardar y usar en otra pantalla
-//     const divCarrito =  document.getElementById('carrito')
-//     divCarrito.innerHTML = "";
-//     for (const producto of carrito) {
-//         let item = document.createElement('p');
-//         item.innerHTML = `<div class="card" style="width: 18rem;">
-//         <div class="card-body">
-//           <h5 class="card-title">Tipo : ${producto.tipo} </h5>
-//           <h5 class="card-title">Talle : ${producto.talle} </h5>
-//           <h5 class="card-title">Color : ${producto.color} </h5>
-//           <h5 class="card-title">Precio : ${producto.precio} </h5>
-//         </div>
-//       </div>`
-//         divCarrito.appendChild(item);
-//     }
-
-// }
-
-// for (const boton of botones) {
-//     boton.addEventListener("click", comprarManejador);  
-// }
-
-
-
-// ----------------------agregue Jquery----------------------------------------------------------------
-
-function productosUIjQuery(productos , id){
-    for (const producto of productos) {
-        $(id).append(`<div class="card" style="width: 18rem;">
+function comprarManejador(e){
+    // console.log(e.target.id);
+    const seleccionado = productos.find( producto => producto.id == this.id);
+    carrito.push(seleccionado);
+    console.log(seleccionado);
+    localStorage.setItem('seleccionado' , JSON.stringify(carrito)); //nos habilita a guardar y usar en otra pantalla
+    const divCarrito =  document.getElementById('carrito')
+    divCarrito.innerHTML = "";
+    for (const producto of carrito) {
+        let item = document.createElement('p');
+        item.innerHTML = `<div class="card" style="width: 18rem;">
         <div class="card-body">
           <h5 class="card-title">Tipo : ${producto.tipo} </h5>
           <h5 class="card-title">Talle : ${producto.talle} </h5>
           <h5 class="card-title">Color : ${producto.color} </h5>
           <h5 class="card-title">Precio : ${producto.precio} </h5>
         </div>
-      </div>`)
+      </div>`
+        divCarrito.appendChild(item);
     }
+
 }
-productosUIjQuery(productos , "#carrito");
+
+for (const boton of botones) {
+    boton.addEventListener("click", comprarManejador);  
+}
+
+
+
+
 
